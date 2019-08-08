2633e6dcee66c7f821e6107bf7d325676dc202c0
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-08 16:08:53 -0400

    Add files via upload

---
+++ ChainChild.java
@@ -0,0 +1,14 @@
+package sesssion3;
+
+public class ChainChild extends ConstrChain {
+	String name;
+	int number;
+	char grade;
+	public ChainChild() {
+		super();
+	}
+	public ChainChild(String name,int number, char grade) {
+		super(name,number);
+		this.grade=grade;
+	}
+}
---
+++ ConstrChain.java
@@ -0,0 +1,17 @@
+package sesssion3;
+
+public class ConstrChain {
+	String name;
+	int number;
+	
+	ConstrChain() {
+		this("",0);
+	}
+	ConstrChain(String name,int number) {
+		this.name=name;
+		this.number=number;
+	}
+	public static void main(String[] args) {
+		
+	}
+}
