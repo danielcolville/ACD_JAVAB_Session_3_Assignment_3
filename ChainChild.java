dcb74369e7f36b56908adb5967a3ee863fad4600
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-08 20:22:58 -0400

    Update ChainChild.java

--- ChainChild.java
+++ ChainChild.java
@@ -6,9 +6,11 @@ public class ChainChild extends ConstrChain {
 	char grade;
 	public ChainChild() {
 		super();
+		System.out.println("Child's no arg constructor");
 	}
 	public ChainChild(String name,int number, char grade) {
 		super(name,number);
+		System.out.println("Child's main constructor");
 		this.grade=grade;
 	}
 }
